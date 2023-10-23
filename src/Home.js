df948d12bec79d19edb88c340e7907457e39f811
MastewalB <se.mastewal.belete@gmail.com>
2023-10-24 00:52:46 +0300

    Basic Home Page Setup

---
+++ src/Home.js
@@ -0,0 +1,11 @@
+
+const Home = () => {
+    return (
+        <div className="home-component">
+            <button className="upload-button">Upload Image</button>
+
+        </div>
+    );
+}
+
+export default Home;
---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>Photo Enhancer</h1>
+            <div className="links">
+                {/* <Link to="/">Home</Link> */}
+                {/* <Link to="/create">New Blog</Link> */}
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
