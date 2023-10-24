8fb0b645cf32673dc4ee045a27ab71ef6ac40d26
MastewalB <se.mastewal.belete@gmail.com>
2023-10-25 01:18:27 +0300

    Pick Image from Local Computer

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,17 @@
+import { useState } from "react";
 
 const Home = () => {
+    const [file, setFile] = useState();
+
+    const handleFileChange = (e) => {
+        setFile(URL.createObjectURL(e.target.files[0]));
+    }
+
     return (
         <div className="home-component">
-            <button className="upload-button">Upload Image</button>
-
+            <input type="file" id="actual-btn" onChange={handleFileChange} hidden />
+            <label for="actual-btn" className="upload-button">Choose File</label>
+            <img src={file} alt={file} />
         </div>
     );
 }
